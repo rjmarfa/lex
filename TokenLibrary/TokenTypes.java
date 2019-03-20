7cb70e79bc7d8ac378ddd6244a920870572497eb
rjmarfa <48747569+rjmarfa@users.noreply.github.com>
2019-03-20 14:36:35 +0800

    Add files via upload

---
+++ TokenLibrary/TokenTypes.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TokenLibrary;
+
+/**
+ *
+ * @author RJ
+ */
+public enum TokenTypes {
+    NEW_LINE,
+    TAB,
+    HOLE,
+    ID,
+    FOLL,
+    LENGTH,
+    SUBS,
+    DESC_DATA,
+    INIT_DATA,
+    STAT_DATA,
+    FRAC_DATA,
+    NAME_DATA,
+    DESC_VAR,
+    INIT_VAR,
+    STAT_VAR,
+    FRAC_VAR,
+    NAME_VAR,
+    FRAC_LIT,
+    STAT_LIT,
+    INIT_LIT,
+    NAME_LIT,
+    DESC_LITt,
+    DESC_LITf,
+    LOC,
+    DESC,
+    PATHMAIN,
+    COMBO_F,
+    COMBO_W,
+    COMBO_N,
+    CHAIN,
+    
+    CONDITION,
+    ENDLINE,
+    RELOP,
+    PLAYER_O,
+    PLAYER_F,
+    GAME_START,
+    GAME,
+    ENDGAME,
+    L_PAR,
+    R_PAR,
+    DOTS,
+    SCL,
+    G_EFF,
+    L_EFF,
+    GRADE,
+    ASSIGN,
+    NOT,
+    G_LOG,
+    L_LOG,
+    S_ARRAY,
+    M_ARRAY,
+    LEFTBRACE,
+    RIGHTBRACE,
+    id,
+
+    
+
+    
+
+    //***Data Types***
+    INT_DT,
+    LOG_DT,
+    DECIMAL_DT,
+    SENTENCE_DT,
+    LETTER_DT,
+    CONST_INT_DT,
+    CONST_LOG_DT,
+    CONST_DEC_DT,
+    CONST_SENT_DT,
+    CONST_LETTER_DT,
+    END_GAME,
+    IDToken,
+    COMMENT,
+
+    //***RELATIONAL OPERATORS***
+    MULDIVOP,
+    ADDSUBOP,
+    //RELOP,
+    LOGICALOP,
+    ASSIGN_OP,
+
+    END_LINE,
+    SEPARATOR_ID,
+
+    LETTER_SMBL,
+    STRINGLIT_SMBL,
+    STRINGLITERAL,
+    NUMCONSTANTS,
+    SINGCOMM_SMBL,
+    BOOLLITERAL,
+    LETTERLITERAL,
+
+
+    SINGLECOMMENT,
+    MULTICOMM_SMBL,
+    MULTICOMMENT,
+    SPACETAB,
+   // TAB,
+    NEWLINE,
+    WHITESPACE,
+    ERROR,
+    INDEC_OP,
+
+}
