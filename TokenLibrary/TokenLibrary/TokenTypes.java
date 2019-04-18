a0a6ab73a540b2456e5aba5c502b58b5f1d98087
rjmarfa <48747569+rjmarfa@users.noreply.github.com>
2019-04-18 20:38:53 +0800

    Add files via upload

---
+++ TokenLibrary/TokenLibrary/TokenTypes.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TokenLibrary;
+
+/**
+ *
+ * @author RJ
+ */
+public enum TokenTypes {
+    
+    START,
+    INTEGER,
+    INPUT,
+    OUTPUT,
+    IFMAIN,
+    LPAR,
+    RPAR,
+    LCUR,
+    RCUR,
+    ID,
+    LOOP,
+    END,
+    ENDLINE,
+    SCOMMENT,
+    MCOMMENT,
+    WHITESPACE,
+    ERROR,
+    SELECT,
+    MONEY,
+    CHANGE,
+    DIGIT,
+
+    //***Data Types***
+    INT,
+    STRING,
+    BOOLEAN,
+    BOOLVALUE,
+    COMMENT,
+
+    //***RELATIONAL OPERATORS***
+    MULDIV,
+    ADDSUB,
+    RELOP,
+    G_LOG,
+    L_LOG,
+    ASSIGN,
+
+    
+
+}
